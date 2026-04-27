caac31bca9a847d20730db90a3a087c4deacac89
zl1006 <59389142+zl1006@users.noreply.github.com>
2026-04-28 00:31:57 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+echo "===== 自定义设置 ====="
+sed -i "s/option ipaddr '192.168.1.1'/option ipaddr '192.168.123.1'/g" /etc/config/network
+/etc/init.d/network reload
+uci set wireless.default_radio0.ssid='PDCN'
+uci set wireless.default_radio0.encryption='psk2'
+uci set wireless.default_radio0.key='1234567890'
+uci set wireless.radio0.disabled='0'
+uci set wireless.default_radio1.ssid='PDCN-5G'
+uci set wireless.default_radio1.encryption='psk2'
+uci set wireless.default_radio1.key='1234567890'
+uci set wireless.radio1.disabled='0'
+uci commit wireless
+wifi reload
+echo "===== 设置完成 ====="
